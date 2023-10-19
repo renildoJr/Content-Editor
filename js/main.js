c9d80e315cbb1e34166a35d3b4c691e186a5b58d
RenildoJr <juninho952020@gmail.com>
2023-10-19 20:44:22 -0300

    Versão Inicial pronta.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const btnToolbar = document.querySelectorAll(".btn-toolbar");
+const btnToolbar = document.querySelectorAll(".btn-text");
 const content = document.querySelector('.content');
 const counter = document.getElementById("count");
+const uploadButton = document.getElementById("uploadButton");
 let selectedElement = null;
 
 content.addEventListener("input", function () {
@@ -15,6 +16,29 @@ btnToolbar.forEach(button => button.addEventListener("click", ()=>{
     formatText(button.value);
 }))
 
+content.addEventListener("input", initTextInsideDiv);
+
+content.addEventListener('click', function(event) {
+    const element = event.target;
+    selectedElement = element;
+});
+
+uploadButton.addEventListener("click", ()=> {
+    const image_upload = document.getElementById("image_upload");
+    image_upload.click()
+
+    image_upload.onchange = function() {
+        const newImage = document.createElement("img");
+        newImage.src = URL.createObjectURL(image_upload.files[0]);
+    
+        if(selectedElement == null) {
+            content.appendChild(newImage);
+        }else {
+            selectedElement.appendChild(newImage);
+        }
+    }
+})
+
 function formatText(tag) {
     const selection = window.getSelection();
     const range = selection.getRangeAt(0);
@@ -22,16 +46,12 @@ function formatText(tag) {
     // Verificar se o texto selecionado pertence à div .content
     if (content.contains(range.commonAncestorContainer)) {
         const element = document.createElement(tag);
-        // element.setAttribute("class", 'editable');
         element.textContent = selection.toString();
         range.deleteContents();
         range.insertNode(element);
         content.focus();
     }
 }
-
-
-content.addEventListener("input", initTextInsideDiv)
 
 function initTextInsideDiv() {
     const allContent = content.querySelectorAll("*");
@@ -46,32 +66,15 @@ function initTextInsideDiv() {
             selectedElement = newDiv;
             newDiv.focus();
 
-            // Coloque o foco no final da nova div para que o cursor de texto inicie após o último caractere
             const range = document.createRange();
             const selection = window.getSelection();
-            range.setStart(newDiv, 1); // Começa após o último caractere
+            range.setStart(newDiv, 1);
             range.collapse(true);
             selection.removeAllRanges();
             selection.addRange(range);
         }
     }
-    // else {
-    //     Contém tags html
-    // }
 }
-
-
-// Position (Experimental)
-content.addEventListener('click', function(event) {
-    const element = event.target;
-
-    // if(element.classList.value == "editable") {
-    //     element.style.textAlign = "center"
-    //     console.log(element)
-    // } 
-
-    selectedElement = element;
-});
 
 function changeElementPosition(pos) {
     selectedElement.style.textAlign = pos;
