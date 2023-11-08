9a7cf4390342a44fc8ca5acc1058c4d3385307f6
RenildoJr <juninho952020@gmail.com>
2023-11-08 18:42:28 -0300

    Código Atual

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+console.log(formData)
+
 const btnToolbar = document.querySelectorAll(".btn-text");
 const content = document.querySelector('.content');
 const counter = document.getElementById("count");
@@ -21,21 +23,22 @@ content.addEventListener('click', function(event) {
     selectedElement = element;
 });
 
-// uploadButton.addEventListener("click", ()=> {
-//     const image_upload = document.getElementById("image_upload");
-//     image_upload.click()
+uploadButton.addEventListener("click", ()=> {
+    const image_upload = document.getElementById("image_upload");
+    image_upload.click()
 
-//     image_upload.onchange = function() {
-//         const newImage = document.createElement("img");
-//         newImage.src = URL.createObjectURL(image_upload.files[0]);
+    image_upload.onchange = function() {
+        const newImage = document.createElement("img");
+        newImage.src = URL.createObjectURL(image_upload.files[0]);
     
-//         if(selectedElement == null) {
-//             content.appendChild(newImage);
-//         }else {
-//             selectedElement.appendChild(newImage);
-//         }
-//     }
-// })
+        if(selectedElement == null) {
+            content.appendChild(newImage);
+        }else {
+            selectedElement.appendChild(newImage);
+        }
+
+    }
+})
 
 function formatText(tag) {
     const selection = window.getSelection();
